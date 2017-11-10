d8929760294481c4908ae5d6159c1e9d63abaf37
Julien Rohou <julien.rohou@laposte.net>
2017-11-10 15:06:00 +0100

    robert part

--- 01-rappels-es5/03.poo.js
+++ 01-rappels-es5/03.poo.js
@@ -1,24 +1,42 @@
-function Personne(nom, prenom, pseudo) {
+function Personne(nom, prenom, pseudo,age) {
   this.nom = nom;
   this.prenom = prenom;
   this.pseudo = pseudo;
+  this.age = age;
   this.getNomComplet = function() {
 
     return this.nom+" "+this.prenom+" "+this.pseudo;
   }
+
+  this.getInitiales = function() {
+    return this.nom.charAt(0) + " "+this.prenom.charAt(0);
+  }
 }
 
-var jules = new Personne("Jules","LEMAIRE","jules77");
+var jules = new Personne("Jules","LEMAIRE","jules77","30");
 
 var paul = new Personne("Paul", "LEMAIRE","paul44");
+
+jules.pseudo = "jules44";
 
 
 console.log(jules.nom);
 console.log(jules.prenom);
 console.log(jules.pseudo);
+console.log(jules.age);
 console.log(jules.getNomComplet());
+console.log(jules.getInitiales());
 
 console.log(paul.nom);
 console.log(paul.prenom);
 console.log(paul.pseudo);
 console.log(paul.getNomComplet());
+
+var robert = {prenom:'Robert',nom:'LEPREFET',pseudo:'robert77',getNomComplet:function() {
+  robert.prenom+" "+robert.nom+" "+robert.pseudo;
+}}
+
+console.log(robert.nom);
+console.log(robert.prenom);
+console.log(robert.pseudo);
+console.log(robert.getNomComplet());
